bf8e027807cb7d0c5cbf8e367ce63d8199d55879
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-15 13:13:26 +0100

    Final commit.

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,7 +19,26 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji:"🦾", 
+    name: "Mechanical arm",
+    meaning: "Be as strong as a robot! The high-tech arm prosthesis enables amputees to move their arms and hands. Expresses enthusiasm for Star Wars, robot technology, bionics or science fiction."
+  },
+  {
+    id: 5,
+    emoji:"💄", 
+    name: "Lipstick",
+    meaning: "I'll dress up! The lipstick beautifies millions of women's lips worldwide. The signal color red is associated with passion, liveliness and attractiveness. Stands for parties, fashion and fun. Often also combined with e.g. the dancing emoji 💄💃 to plan a fun evening."
+  },
+  {
+    id: 6,
+    emoji: "🥯",
+    name: "Bagel", 
+    meaning: "I am having an American breakfast today! Palm-sized round yeast pastry with a hole in the middle. Bagels were brought to the United States by Jewish immigrants and are now considered typical American. The topping is either sweet or spicy."
   }
+
 ];
 
 export default emojipedia;
